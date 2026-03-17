3072a4ae87410c0c365ed6d46cc908a6dd13545e
NekoShinobi <1697670+NekoShinobi@users.noreply.github.com>
2026-03-17 02:19:27 -0700

    big changes.

--- home/dot_config/hypr/scripts/executable_screenshot.sh
+++ home/dot_config/hypr/scripts/executable_screenshot.sh
@@ -1,2 +1,15 @@
-!#/bin/zsh
-grim -g "$(slurp)" &
+#!/bin/bash
+activewindow=$(hyprctl activewindow -j | jq '.initialTitle' | sed 's/"//g')
+timestamp=$(date +"%F_%H%M%S")
+
+# Check if homedata directory exists
+if [ ! -d ~/homedata/home ]; then
+  notify-send -u critical "Screenshot Error" "Directory ~/homedata/home does not exist. rclone is probably not running."
+  exit 1
+fi
+
+folder=~/homedata/home/Daniel/Screenshots/$(date +"%Y-%m")
+mkdir -p $folder
+flameshot gui -p $folder/${activewindow}_${timestamp} \
+  -c
+# grimblast --notify --freeze copysave area
--- home/dot_config/hypr/scripts/executable_wbrestart.sh
+++ home/dot_config/hypr/scripts/executable_wbrestart.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
-killall -9 swaync
-killall -9 waybar
-killall -9 linux-wallpaperengine
+killall -9 quickshell
 
-swaync &
-waybar &
-sleep 5 && linux-wallpaperengine 3504312917 --screen-root DP-1 --silent &
+quickshell --config ~/.config/quickshell/custom/ &
+
+# killall -9 linux-wallpaperengine
+# sleep 5 && linux-wallpaperengine 3504312917 --screen-root DP-1 --silent &
